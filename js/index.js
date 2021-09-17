888cb4f1c3df6cfd3f0d27111c303a6f06dd178a
Arundhati <arundhatiraicar12@gmail.com>
2021-09-17 19:04:40 +0530

    commit tree de2f3655dfe8d1e7a88d51e914f560acdd215434

---
+++ js/index.js
@@ -0,0 +1,62 @@
+const form = document.getElementById("create-monster-form")
+console.log(form)
+
+form.addEventListener("submit",(event)=>{
+    event.preventDefault()
+    const [name,age,description]=event.target
+     
+    fetch("http://localhost:3000/monsters",{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify({
+            name: name.value, 
+            age: age.value, 
+            description: description.value
+        })
+    })
+    .then(response=>response.json())
+    .then(response=>console.log(response))
+})
+let pageNumber=1
+
+function fetchMonster(){
+fetch(`http://localhost:3000/monsters?_limit=20&_page=${pageNumber}`)
+.then(response => response.json())
+.then(response =>  response.forEach(r => showMonster(r)))
+}
+fetchMonster()
+function showMonster(monster) {
+    const monsterContainer = document.querySelector('#monster-container')
+    const h1 = document.createElement("h1")
+    h1.textContent = monster.name
+    const h2 = document.createElement("h2")
+    h2.textContent = monster.age
+    const h3 = document.createElement("h3")
+    h3.textContent = monster.description
+    monsterContainer.append(h1, h2, h3)
+}
+document.addEventListener("click", (event) => {
+    if(event.target.matches("button#forward")) {
+        event.preventDefault()
+        handleFrontClick()
+    }
+    if(event.target.matches("#back")) {
+        handleBackClick()
+    }
+})
+
+const handleFrontClick = () => {
+    pageNumber += 1
+    const monsterContainer = document.querySelector('#monster-container')
+    monsterContainer.innerHTML = ""
+    fetchMonster()
+
+}
+const handleBackClick=()=>{
+    pageNumber-=1
+    const monsterContainer=document.querySelector("#monster-container")
+    monsterContainer.innerHTML=""
+    fetchMonster()
+}
